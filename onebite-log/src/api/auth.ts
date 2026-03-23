adaaecfe319136565ae2b602f1d18661fdeaaa94
신민경 <80gaeng@hanmail.net>
2026-03-24 08:42:35 +0900

    비밀번호 재설정 공부중

--- onebite-log/src/api/auth.ts
+++ onebite-log/src/api/auth.ts
@@ -39,3 +39,11 @@ export async function signInWithOAuth(provider: Provider) {
   if (error) throw error;
   return data;
 }
+
+export async function requestPasswordResetEmail({ email }: { email: string }) {
+  const { data, error } = await supabase.auth.resetPasswordForEmail(email, {
+    redirectTo: `${import.meta.env.VITE_PUBLIC_URL}/reset-password`,
+  });
+  if (error) throw error;
+  return data;
+}
--- onebite-log/src/pages/forgot-password-page.tsx
+++ onebite-log/src/pages/forgot-password-page.tsx
@@ -1,3 +1,31 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+
 export default function ForgotPasswordPage() {
-  return <div>ForgotPasswordPage</div>;
+  const [email, setEmail] = useState("");
+  const handleSendEmailClick = () => {
+    if (email.trim() === "") {
+      return;
+    }
+  };
+  return (
+    <div className="flex flex-col gap-8">
+      <div className="flex flex-col gap-2">
+        <div className="text-xl font-bold">비밀번호를 잊으셨나요?</div>
+        <div className="text-muted-foreground">
+          이메일로 비밀번호를 재설정 할 수 있는 인증 링크를 보내드립니다.
+        </div>
+      </div>
+      <Input
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        className="py-6"
+        placeholder="example@abc.com"
+      />
+      <Button className="w-full" onClick={handleSendEmailClick}>
+        인증 메일 요청하기
+      </Button>
+    </div>
+  );
 }
